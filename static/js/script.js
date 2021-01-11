1525159f1f9dbd9376526122be2281a210b008cf
d-mcalpin <duncan.mcalpin@gmail.com>
2021-01-11 18:18:37 +0000

    Added javascript code for handling form and date picker

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,13 @@
  $(document).ready(function(){
     $('.sidenav').sidenav({edge: "right"});
+    $('select').formSelect();
+    $('.datepicker').datepicker({
+        format: "dd mmm yyyy",
+        yearRange: 0,
+        showClearBtn: true,
+        i18n:{
+            done: "Select"
+        }
+    });
+    $('.modal').modal();
   });
